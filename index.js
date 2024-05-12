cf3448b206d4bbc4cc5eb53493af9054a3dc57d2
AdnaNation <azizulislamadnan@gmail.com>
2024-05-12 11:36:31 +0600

    made read operation for getting service detail

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
@@ -37,12 +37,19 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/popularService',async (req, res)=>{
+    app.get('/services',async (req, res)=>{
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         // console.log(result)
         res.send(result);
     })
+
+    app.get('/services/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
